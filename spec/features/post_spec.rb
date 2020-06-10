232a3f8941df5c1d2ee924fce58da0cf15263023
Elijah wines <elijahwines84@yahoo.com>
2020-06-10 13:18:48 +0000

    Automatically backed up by Learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
+require 'rails_helper'
+
 describe 'navigate' do
   before do
     @post = Post.create(title: "My Post", description: "My post desc")
@@ -8,5 +10,10 @@ describe 'navigate' do
   it 'loads the show page' do
     visit "/posts/#{@post.id}"
     expect(page.status_code).to eq(200)
+  end
+
+  it 'shows the title on the show page in an h1 tag' do
+    visit "/posts/#{@post.id}"
+    expect(page).to have_css("h1", text: "My Post")
   end
 end
